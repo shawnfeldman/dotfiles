612321dbb9d4e3a138386b215c8c146b8de0fa3d
Shawn Feldman <shawnfeldman@github.com>
2021-09-23 09:20:05 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ set -x
 
 #git config --global core.editor "vim"
 #touch ~/gotmydots.txt
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip -d 
+mkdir .ngrok2
+touch .ngrok2/ngrok.yml
 
 source .bash_profile
 source .zshrc
