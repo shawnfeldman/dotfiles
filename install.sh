f78dc57fbf1a5505257a8a795edb5c089f833c48
Shawn Feldman <shawnfeldman@github.com>
2021-10-27 10:48:13 -0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+set -x
+
+source .bash_profile
+source .zshrc
+
+if [[ -d "/workspaces/github" ]]; then
+  export IS_GH_GH=true
+  cd "/workspaces/github"
+  bin/setup-codespaces
+fi
