a9311653d7f1dee7d29198d1f2b1b7affa87cccb
Shawn Feldman <shawnfeldman@github.com>
2021-10-27 16:59:07 +0000

    hide defaults

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
-
 #!/bin/bash
 
 set -x
 
-source .bash_profile
-source .zshrc
+# source ~/.bash_profile
+# source ~/.zshrc
 
-if [[ -d "/workspaces/github" ]]; then
-  export IS_GH_GH=true
-  cd "/workspaces/github"
-  bin/setup-codespaces
-fi
+# if [[ -d "/workspaces/github" ]]; then
+#   export IS_GH_GH=true
+#   cd "/workspaces/github"
+#   bin/setup-codespaces
+# fi
